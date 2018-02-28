97ed48c3ac7e225c73d23aba2100a9917e244a17
Mik@ <michael.ledour@gmail.com>
2018-02-28 15:11:03 +0100

    demo

--- demo/src/app/chart-donut/morris-js-donut.ts
+++
@@ -1,9 +0,0 @@
-export var chartDonutOptions = {
-  resize: true
-};
-
-export var chartDonutData =  [
-  {label: "Download Sales", value: 12},
-  {label: "In-Store Sales", value: 30},
-  {label: "Mail-Order Sales", value: 20}
-];
